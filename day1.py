7e286f001905adf1529d5b7bea101ad67440fa4f
Michael-Ralph <MichaelR@praxima.com>
2025-05-22 08:19:05 +0200

    days 1-3 added

---
+++ day1.py
@@ -0,0 +1,21 @@
+from math import pi
+
+age = 30
+print(age)
+
+days_in_a_year = 365
+weeks_in_a_year = 52
+months_in_a_year = 12
+year = 27
+
+days = year * days_in_a_year
+weeks = year * weeks_in_a_year
+months = year * months_in_a_year
+
+print(f"There are {months} months, {weeks} weeks and {days} days in {year} years.")
+
+r = 5
+
+area = pi * r ** 2
+
+print(area)
---
+++ day2.py
@@ -0,0 +1,31 @@
+#name = input("What is your name?: ")
+#age = input("How old are you?: ")
+
+#print(name)
+#print(age)
+
+#name = "Spudtastico"
+
+#print(name)
+
+# Code before being edited
+
+"""
+hourly_wage = input("Please enter your hourly wage: ')
+
+prnt("Hourly wage: ")
+print(hourlywage)
+print("Hours worked: ")
+print(hours_worked)
+
+hours_worked = input("How many hours did you work this week? ")
+"""
+hourly_wage = input("Please enter your hourly wage: ")
+hours_worked = input("How many hours did you work this week? ")
+
+print("Hourly wage: ")
+print(hourly_wage)
+print("Hours worked: ")
+print(hours_worked)
+
+
