2ea802c25cc5d9d35ccc145ac467af0e36bd46fa
Michael-Ralph <MichaelR@praxima.com>
2025-05-27 07:44:33 +0200

    day 3 done

---
+++ day3.py
@@ -0,0 +1,19 @@
+#greeting = "Hello, world{0}"
+
+#print(greeting.format("!"))
+
+#name = input("What is your name?: ").strip().title()
+#greeting = "Hello, {0}!"
+
+#print(greeting.format(name))
+
+#my_age = "I am "
+
+#print(my_age + str(29))
+
+title = "Joker"
+director = "Todd Phillips"
+release_year = 2019
+
+output = f"{title} ({release_year}), directed by {director}."
+print(output)
